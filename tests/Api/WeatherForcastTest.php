87b4a5cfdb1577224e1a0164a1593b576edbdaad
Stephan Tijink <stijink@googlemail.com>
2017-02-28 22:56:44 +0100

    Test Setup

---
+++ tests/Api/WeatherForcastTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Api;
+
+use Api\WeatherForcast;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+class WeatherForcastTest extends TestCase
+{
+    public function testLoad()
+    {
+        $mockedHttpClient = \Mockery::mock('GuzzleHttp\Client')
+            ->shouldReceive('get')
+            ->once()
+            ->withArgs([
+                'http://example-api.com',
+                [
+                    'query' => [
+                        'q' => 'Troisdorf',
+                        'APPID' => 1234,
+                        'units' => 'metric',
+                        'lang'  => 'de',
+                        'cnt'   => 5,
+                    ]
+                ]
+            ])
+            ->andReturn($this->exampleResponse())
+            ->getMock();
+
+        $weatherForcast = new WeatherForcast($mockedHttpClient, [
+            'api_url' => 'http://example-api.com',
+            'api_key' => 1234,
+            'city'    => 'Troisdorf'
+        ]);
+
+        $response = $weatherForcast->load();
+
+        $this->assertInternalType('array', $response);
+        $this->assertCount(5, $response);
+    }
+
+    private function exampleResponse()
+    {
+        $body = file_get_contents(__DIR__ .'/Fixtures/weather-forcast.json');
+        return new Response(200, [], $body);
+    }
+}
