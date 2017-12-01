0d25a01f9cf9d84436cfd707f19d6b633ee2bd5a
Stephan Tijink <stijink@googlemail.com>
2017-12-01 14:42:52 +0100

    Pull latest versions of docker dependencies on build

--- bin/build-dev.sh
+++ bin/build-dev.sh
@@ -5,6 +5,10 @@ if [ ! -f api/config.php ]; then
     cp api/config.php.dist api/config.php
 fi
 
+# Pull latest versions of docker dependencies
+docker pull php:7.1-apache
+docker pull node:8
+
 # Ensure images are built
 docker-compose build
 
