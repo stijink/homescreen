6133e4b0265a2118769e1c4032af5529a6a24efe
Stephan Tijink <stijink@googlemail.com>
2019-04-12 13:05:17 +0200

    Fix for Raspberries

--- app/src/Component/Raspberries.vue
+++ app/src/Component/Raspberries.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container fluid class="pa-0">
+    <v-container fluid class="pa-0" v-if="raspberries">
 
         <v-layout row v-for="raspberry in raspberries" :key="raspberry.hostname" class="mb-5">
 
