cc3b59dd88361f6a44f85297fd5ba50952317b21
Stephan Tijink <stijink@googlemail.com>
2021-11-19 15:30:19 +0100

    incidenceColor

--- app/src/Component/Covid19.vue
+++ app/src/Component/Covid19.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="covid19" class="text-xs-right pr-4">
 
-        <h1>{{ weekIncidence }}</h1>
+        <h1 :style="incidenceColor">{{ weekIncidence }}</h1>
         <strong>Covid 19 Inzidenz für den {{ location }} </strong>
 
     </div>
@@ -20,6 +20,49 @@
 
                 location: null,
                 weekIncidence: null,
+            }
+        },
+        computed: {
+            incidenceColor: function() {
+
+                if (this.weekIncidence < 4) {
+                     return 'color: #ffff6d';
+                }
+
+                if (this.weekIncidence < 20) {
+                     return 'color: #f6df35';
+                }
+
+                if (this.weekIncidence < 50) {
+                     return 'color: #cb2e2b';
+                }
+
+                if (this.weekIncidence < 100) {
+                     return 'color: #920002';
+                }
+
+                if (this.weekIncidence < 200) {
+                     return 'color: #5e0000';
+                }
+
+                if (this.weekIncidence < 400) {
+                     return 'color: #400000';
+                }
+
+                if (this.weekIncidence < 600) {
+                     return 'color: #a84693';
+                }
+
+                if (this.weekIncidence < 800) {
+                     return 'color: #832b79';
+                }
+
+                if (this.weekIncidence < 1000) {
+                     return 'color: #611d5a';
+                }
+
+                // >= 1000
+                return 'color: #41143c';
             }
         },
         methods: {
